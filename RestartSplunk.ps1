4200ab5118022606a7cf82a8e2b914d0da3c30d1
GVegas44 <47002380+GVegas44@users.noreply.github.com>
2020-12-17 12:59:02 -0500

    Add files via upload

---
+++ RestartSplunk.ps1
@@ -0,0 +1,11 @@
+﻿$creds = Get-Credential -Credential meetech.com\james.bond
+$ip = 10,11,12,13,14,16,18,2,22,23,24,25,26,27,28,29,30,37,4,40,5,6,7,8,9
+
+
+
+
+forEach ($i in $ip) {
+    $session = New-PSSession -computername 11.25.20.$i -Credential $creds
+    Invoke-Command -session $session -ScriptBlock { get-Service SplunkForwarder }
+    Remove-PSSession -Session $session
+}
---
+++ SplunkForwarderStatus.ps1
@@ -0,0 +1,11 @@
+﻿$creds = Get-Credential -Credential meetech.com\james.bond
+$ip = 2,4,5,6,7
+
+
+
+
+forEach ($i in $ip) {
+    $session = New-PSSession -computername 11.25.15.$i -Credential $creds
+    Invoke-Command -session $session -ScriptBlock { Get-Service SplunkForwarder }
+    Remove-PSSession -Session $session
+}
